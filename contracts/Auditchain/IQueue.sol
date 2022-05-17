a0962593fb073873b8907fcf565aaf51d9f949a8
win2win <win2win@protonmail.com>
2022-05-17 09:34:08 -0500

    clean up

--- contracts/Auditchain/IQueue.sol
+++ contracts/Auditchain/IQueue.sol
@@ -3,9 +3,9 @@ pragma solidity =0.8.0;
     
 interface IQueue {
 
-
     function addToQueue(uint256 _price, bytes32 _validationHash) external returns(bool);
     function removeFromQueue(bytes32 _valHash) external returns (bool);
     function setValidatedFlag(bytes32 _valHash)  external returns(bool);
     function replaceValidation(uint256 newPrice, bytes32 _valHash) external;
+
 }
