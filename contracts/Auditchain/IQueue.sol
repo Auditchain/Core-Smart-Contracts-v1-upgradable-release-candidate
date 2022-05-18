7a22574fc5c80de878d0469d8f95bd34fb1f4a07
win2win <win2win@protonmail.com>
2022-05-17 20:05:29 -0500

    add return

--- contracts/Auditchain/IQueue.sol
+++ contracts/Auditchain/IQueue.sol
@@ -6,6 +6,6 @@ interface IQueue {
     function addToQueue(uint256 _price, bytes32 _validationHash) external returns(bool);
     function removeFromQueue(bytes32 _valHash) external returns (bool);
     function setValidatedFlag(bytes32 _valHash)  external returns(bool);
-    function replaceValidation(uint256 newPrice, bytes32 _valHash) external;
+    function replaceValidation(uint256 newPrice, bytes32 _valHash) external returns(bool);
 
 }
