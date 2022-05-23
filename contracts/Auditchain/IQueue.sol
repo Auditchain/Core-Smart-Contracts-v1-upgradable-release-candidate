6a8b764c41b25323f824e178d43884fc6647d6d3
win2win <win2win@protonmail.com>
2022-05-23 12:13:48 -0500

    add initTime

--- contracts/Auditchain/IQueue.sol
+++ contracts/Auditchain/IQueue.sol
@@ -3,9 +3,9 @@ pragma solidity =0.8.0;
     
 interface IQueue {
 
-    function addToQueue(uint256 _price, bytes32 _validationHash) external returns(bool);
-    function removeFromQueue(bytes32 _valHash) external returns (bool);
-    function setValidatedFlag(bytes32 _valHash)  external returns(bool);
-    function replaceValidation(uint256 newPrice, bytes32 _valHash) external returns(bool);
+    function addToQueue(uint256 price, bytes32 validationHash, bytes32 documentHash, string memory url, address user,  uint256 initTime) external returns(bool);
+    function removeFromQueue(bytes32 valHash) external returns (bool);
+    function setValidatedFlag(bytes32 valHash)  external returns(bool);
+    function replaceValidation(uint256 newPrice, bytes32 valHash) external returns(bool);
 
 }
