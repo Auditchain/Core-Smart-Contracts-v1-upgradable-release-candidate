30f2d5343d10c0391246ffa2bba097854a021fee
win2win <win2win@protonmail.com>
2022-05-27 13:39:10 -0500

    remove max contribution

--- contracts/Auditchain/MemberHelpers.sol
+++ contracts/Auditchain/MemberHelpers.sol
@@ -21,7 +21,6 @@ contract MemberHelpers is AccessControlEnumerableUpgradeable, ReentrancyGuardUpg
     IValidations public validations; // Validation interface
     mapping(address => uint256) public deposits; //track deposits per user
     uint256 public minContribution;
-    uint256 public maxContribution;
     uint256 public totalStaked;
     
 
@@ -34,7 +33,6 @@ contract MemberHelpers is AccessControlEnumerableUpgradeable, ReentrancyGuardUpg
         require(_members != address(0),"MemberHelpers:constructor - Member address can't be 0");
         require(_auditToken != address(0), "MemberHelpers:setCohort - Cohort address can't be 0");
         minContribution = 5e21;
-        maxContribution = 25e21;
         members = Members(_members);
         auditToken = _auditToken;
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
