475bad5428cfa2d3156b8f87ee066acaf9b95d31
win2win <win2win@protonmail.com>
2022-08-09 09:56:09 -0500

    add get, id , find PrevId

--- contracts/Auditchain/IQueue.sol
+++ contracts/Auditchain/IQueue.sol
@@ -7,5 +7,8 @@ interface IQueue {
     function removeFromQueue(bytes32 valHash) external returns (bool);
     function setValidatedFlag(bytes32 valHash)  external returns(bool);
     function replaceValidation(uint256 newPrice, bytes32 valHash) external returns(bool);
-
+    function returnQueueSize() external view returns(uint256);
+    function get(uint256 _id) external view returns
+    (uint256 id, uint256 next, uint256 price, bytes32 validationHash, bytes32 documentHash, string memory url, address user, uint256 initTime, bool executed);
+    function findPrevId(uint256 _id) external view returns (uint256);
 }
