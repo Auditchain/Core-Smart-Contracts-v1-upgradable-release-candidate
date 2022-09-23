6fbc087fe6757d34fb063f61cb45bea2622db285
win2win <win2win@protonmail.com>
2022-09-22 21:15:31 -0500

    include auditType in addToQueue

--- contracts/Auditchain/IQueue.sol
+++ contracts/Auditchain/IQueue.sol
@@ -3,7 +3,7 @@ pragma solidity =0.8.0;
     
 interface IQueue {
 
-    function addToQueue(uint256 price, bytes32 validationHash, bytes32 documentHash, string memory url, address user,  uint256 initTime) external returns(bool);
+    function addToQueue(uint256 price, bytes32 validationHash, bytes32 documentHash, string memory url, address user,  uint256 initTime, uint8 auditType) external returns(bool);
     function removeFromQueue(bytes32 valHash) external returns (bool);
     function setValidatedFlag(bytes32 valHash)  external returns(bool);
     function replaceValidation(uint256 newPrice, bytes32 valHash) external returns(bool);
