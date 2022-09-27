e6a33169307518d6fa44ece7e2bd58db9354e8d1
win2win <win2win@protonmail.com>
2022-09-27 17:52:54 -0500

    remove extra functions

--- contracts/Auditchain/ICohortFactory.sol
+++ contracts/Auditchain/ICohortFactory.sol
@@ -3,11 +3,7 @@ pragma solidity =0.8.0;
     
 interface ICohortFactory {
 
-    function returnCohorts(address enterprise) external view returns (address[] memory, uint256[] memory);
-    function returnValidatorList(address enterprise, uint256 audit)external view returns(address[] memory);
-    function cohortMap(address user, uint256 audit)external view returns(bool);
-    function isValidatorInvited(address requestor, address validator, uint256 audit)external view returns(bool, bool);
-    function validatorCohortList(address user, address validator)external view returns(uint256);
-    function returnValidatorCohortsCount(address validator, address enterprise) external view returns (uint256);
+    function returnValidatorList(address enterprise, uint8 audit)external view returns(address[] memory);
+    function returnValidatorCohortsList(address validator, address enterprise) external view returns (uint256[] memory);
 
 }
