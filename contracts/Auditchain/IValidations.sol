272ac0c5e8fdc4cb618c259bc7294d4dc65ddd65
win2win <win2win@protonmail.com>
2022-10-06 23:27:15 -0500

    remove returnValidatorList

--- contracts/Auditchain/IValidations.sol
+++ contracts/Auditchain/IValidations.sol
@@ -9,7 +9,6 @@ interface IValidations {
     function returnValidationUrl(bytes32 validationHash, address user) external  view returns(string memory url);
     function returnWinnerPoints(bytes32 validationHash, address user) external  view returns(uint256 plus, uint256 minus);
     function selectWinner(bytes32 validationHash, address[] memory winners) external view returns (address);
-    function returnValidatorList(bytes32 validationHash) external view  returns (address[] memory);
     function validations(bytes32 valHash) external view returns( uint8 auditTypes,
                                                                 address requestor,
                                                                 uint256 validationTime,
