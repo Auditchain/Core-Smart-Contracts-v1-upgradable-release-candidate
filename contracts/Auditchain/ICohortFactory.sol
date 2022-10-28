0a1900e61faf16d5b7f1f558969728f13d60f859
win2win <win2win@protonmail.com>
2022-10-28 14:36:47 -0500

    add isValidatorInvited

--- contracts/Auditchain/ICohortFactory.sol
+++ contracts/Auditchain/ICohortFactory.sol
@@ -5,5 +5,7 @@ interface ICohortFactory {
 
     function returnValidatorList(address enterprise, uint8 audit)external view returns(address[] memory);
     function returnValidatorCohortsList(address validator, address enterprise) external view returns (uint256[] memory);
+    function isValidatorInvited(address enterprise, address validator, uint256 audits) external view returns (bool invited, bool accepted);
+
 
 }
