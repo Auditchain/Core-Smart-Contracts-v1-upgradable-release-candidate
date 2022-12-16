dd53a028d2924f0cbb4caf6ca41ee67146d82998
win2win <win2win@protonmail.com>
2022-12-16 09:23:14 -0600

    remove verifyInit

--- contracts/Auditchain/IValidationHelpers.sol
+++ contracts/Auditchain/IValidationHelpers.sol
@@ -6,6 +6,6 @@ interface IValidationHelpers {
     function selectWinner(bytes32 validationHash, address[] memory winners) external view returns (address);
     function returnConsensus(bytes32 validationHash, address validationContract) external view returns(uint256);
     function verifyValidate(bool valTime, bool choice, bool userType, address caller) external view returns (bool);
-    function verifyInit(bool docSize, uint256 price, bool userType, address caller) external view returns (bool);
+    // function verifyInit(bool docSize, uint256 price, bool userType, address caller) external view returns (bool);
 
 }
