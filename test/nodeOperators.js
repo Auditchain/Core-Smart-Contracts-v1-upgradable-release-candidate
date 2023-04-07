7f882a465da87d5b768c6fb61549ac6d99218a88
win2win <win2win@protonmail.com>
2023-04-07 16:36:50 -0500

    adjust name

--- test/nodeOperators.js
+++ test/nodeOperators.js
@@ -8,7 +8,7 @@ const TOKEN = artifacts.require('../AuditToken');
 const MEMBER_HELPERS = artifacts.require('../MemberHelpers');
 const NODE_OPERATIONS = artifacts.require('../NodeOperations');
 
-const VALIDATION = artifacts.require('../ValidationsNoCohort');
+const VALIDATION = artifacts.require('../ValNoCohort');
 const VALIDATION_HELPERS = artifacts.require('../ValidationHelpers');
 const QUEUE = artifacts.require("../Queue");
 
